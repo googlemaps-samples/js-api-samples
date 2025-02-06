e2af385b56c7d9a66b0fe4eb02064cb66e281375
William French <wfrench@google.com>
2025-02-06 10:16:13 -0800

    fix: Adds a script to generate the default index file for Firebase.

---
+++ samples/generate-index.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Generate a new index.html for Firebase App Hosting.
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)" # Script directory (/samples)
+PROJECT_ROOT=$(dirname "$SCRIPT_DIR")  # Get the parent directory (js-api-samples)
+DIST_DIR="$PROJECT_ROOT/dist"
+
+# Create the output file.
+OUTPUT_FILE="index.html"
+
+# Generate the HTML document.
+echo "<!DOCTYPE html>" > "${OUTPUT_FILE}"
+echo "<html>" >> "${OUTPUT_FILE}"
+echo "<head>" >> "${OUTPUT_FILE}"
+echo "<title>Maps JSAPI Samples</title>" >> "${OUTPUT_FILE}"
+echo "</head>" >> "${OUTPUT_FILE}"
+echo "<body>" >> "${OUTPUT_FILE}"
+echo "<!-- Default top-level index for Firebase Hosting -->" >> "${OUTPUT_FILE}"
+echo "<h1>Maps JSAPI Samples</h1>" >> "${OUTPUT_FILE}"
+echo "<ul>" >> "${OUTPUT_FILE}"
+
+# Iterate through sample directories.
+find "${SCRIPT_DIR}" -maxdepth 1 -mindepth 1 -type d | while read -r subdir; do
+
+# Extract the directory name.
+DIR_NAME=$(basename "${subdir}")
+
+# Construct the link.
+LINK_URL="${DIR_NAME}/app/dist/"
+LINK_TEXT="${DIR_NAME}"
+
+# Create the list item.
+echo "  <li><a href='${LINK_URL}'>${LINK_TEXT}</a></li>" >> "${OUTPUT_FILE}"
+done
+
+echo "</ul>" >> "${OUTPUT_FILE}"
+echo "</body>" >> "${OUTPUT_FILE}"
+echo "</html>" >> "${OUTPUT_FILE}"
+
+echo "HTML file generated: ${OUTPUT_FILE}"
+
+echo "from ${SCRIPT_DIR}/${OUTPUT_FILE}"
+echo "to ${DIST_DIR}/${OUTPUT_FILE}"
+
+cp "${PROJECT_ROOT}/${OUTPUT_FILE}" "${DIST_DIR}/${OUTPUT_FILE}"
