ba353c505744548f4e06d8086c63e82363863f00
William French <wfrench@google.com>
2025-02-06 10:04:13 -0800

    fix: Updates build workflow to include clean; updates clean.sh to not delete dist from subfolders. (#64)

--- samples/clean.sh
+++ samples/clean.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Remove the global dist folder.
-rm -rf "../dist"
-
-# Remove dist folders from each sample subdirectory.
-find * -type d -name "dist" -exec rm -rf {} +
+# Remove the global dist/samples folder.
+rm -rf "dist/samples"
