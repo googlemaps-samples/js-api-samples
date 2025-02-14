a254f2917be077e4d29c3c6e9a9d20f839454f45
William French <wfrench@google.com>
2025-02-14 15:38:30 -0800

    Update jsfiddle.sh
    I don't wanna talk about it.

--- samples/jsfiddle.sh
+++ samples/jsfiddle.sh
@@ -11,35 +11,34 @@
 NAME=$1 # The name of the folder, taken from package.json "build" line.
 
 # /Users/[USERNAME]/git/js-api-samples/samples or similar
-SAMPLES_DIR="$(cd "$(dirname "$0")" && pwd)" # Script directory (/samples)
+SAMPLES_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # Script directory (/samples)
 PROJECT_ROOT=$(dirname "$SAMPLES_DIR")  # Get the parent directory (js-api-samples)
 DIST_DIR="${PROJECT_ROOT}/dist"
 
-echo "SAMPLES_DIR: ${SAMPLES_DIR}"
-echo "PROJECT_ROOT: ${PROJECT_ROOT}"
-echo "DIST_DIR: ${DIST_DIR}"
-
-echo "${SAMPLES_DIR}/${NAME}"
-
 # Create a new folder.
-mkdir -p ${DIST_DIR}
+mkdir -p "${DIST_DIR}/samples/${NAME}/jsfiddle"
 
 # Copy files
+echo "Copy ${SAMPLES_DIR}/${NAME}/index.js to ${DIST_DIR}/samples/${NAME}/jsfiddle/index.js"
 cp "${SAMPLES_DIR}/${NAME}/index.js" "${DIST_DIR}/samples/${NAME}/jsfiddle/index.js"
 cp "${SAMPLES_DIR}/${NAME}/index.html" "${DIST_DIR}/samples/${NAME}/jsfiddle/index.html"
 cp "${SAMPLES_DIR}/${NAME}/style.css" "${DIST_DIR}/samples/${NAME}/jsfiddle/style.css"
 
 # Remove region tags from files by type, since they all have different comment conventions.
+echo "Remove region tags from ${DIST_DIR}/samples/${NAME}/jsfiddle/index.js"
 sed -i "" "s/\/\/ \[START .*]//g" "${DIST_DIR}/samples/${NAME}/jsfiddle/index.js"
 sed -i "" "s/\/\/ \[END .*]//g" "${DIST_DIR}/samples/${NAME}/jsfiddle/index.js"
 
+echo "Remove region tags from ${DIST_DIR}/samples/${NAME}/jsfiddle/index.html"
 sed -i "" "s/<!--\s*\[START .*\]\s*-->//g" "${DIST_DIR}/samples/${NAME}/jsfiddle/index.html"
 sed -i "" "s/<!--\s*\[END .*\]\s*-->//g" "${DIST_DIR}/samples/${NAME}/jsfiddle/index.html"
 
+echo "Remove region tags from ${DIST_DIR}/samples/${NAME}/jsfiddle/style.css"
 sed -i "" "s/\/\* \[START maps_.*] \*\///g" "${DIST_DIR}/samples/${NAME}/jsfiddle/style.css"
 sed -i "" "s/\/\* \[END maps_.*] \*\///g" "${DIST_DIR}/samples/${NAME}/jsfiddle/style.css"
 
 # Generate demo.details.
+echo "Generate ${DIST_DIR}/samples/${NAME}/jsfiddle/demo.details"
 touch "${DIST_DIR}/samples/${NAME}/jsfiddle/demo.details"
 cat > "${DIST_DIR}/samples/${NAME}/jsfiddle/demo.details" << EOF
 name: ${NAME}
