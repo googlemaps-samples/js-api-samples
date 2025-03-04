2ff360343395780c142b2dffaf05ff1d2d35b3c3
William French <wfrench@google.com>
2025-03-04 14:13:35 -0800

    fix: Removes .js files; updates .gitignore; various small fixes. (#247)

--- samples/generate-index.sh
+++ samples/generate-index.sh
@@ -15,6 +15,14 @@ echo "PROJECT_ROOT: ${PROJECT_ROOT}"
 # Create the output file.
 OUTPUT_FILE="index.html"
 
+# Path to the index.html file
+INDEX_FILE="${PROJECT_ROOT}/${OUTPUT_FILE}"
+
+# Delete the existing index.html file if it exists
+if [ -f "$INDEX_FILE" ]; then
+    rm "$INDEX_FILE"
+fi
+
 # Generate the HTML document.
 echo "<!DOCTYPE html>" > "${OUTPUT_FILE}"
 echo "<html>" >> "${OUTPUT_FILE}"
@@ -27,17 +35,17 @@ echo "<h1>Maps JSAPI Samples</h1>" >> "${OUTPUT_FILE}"
 echo "<ul>" >> "${OUTPUT_FILE}"
 
 # Iterate through sample directories.
-find "${SCRIPT_DIR}" -maxdepth 1 -mindepth 1 -type d | while read -r subdir; do
+find "${SCRIPT_DIR}" -maxdepth 1 -mindepth 1 -type d | sort | while read -r subdir; do
 
-# Extract the directory name.
-DIR_NAME=$(basename "${subdir}")
+  # Extract the directory name.
+  DIR_NAME=$(basename "${subdir}")
 
-# Construct the link.
-LINK_URL="/samples/${DIR_NAME}/dist"
-LINK_TEXT="${DIR_NAME}"
+  # Construct the link.
+  LINK_URL="/samples/${DIR_NAME}/dist"
+  LINK_TEXT="${DIR_NAME}"
 
-# Create the list item.
-echo "  <li><a href='${LINK_URL}'>${LINK_TEXT}</a></li>" >> "${OUTPUT_FILE}"
+  # Create the list item.
+  echo "  <li><a href='${LINK_URL}'>${LINK_TEXT}</a></li>" >> "${OUTPUT_FILE}"
 done
 
 echo "</ul>" >> "${OUTPUT_FILE}"
@@ -49,4 +57,4 @@ echo "HTML file generated: ${OUTPUT_FILE}"
 echo "from ${PROJECT_ROOT}/${OUTPUT_FILE}"
 echo "to ${DIST_DIR}/${OUTPUT_FILE}"
 
-echo cp "${PROJECT_ROOT}/${OUTPUT_FILE}" "${DIST_DIR}/${OUTPUT_FILE}"
+cp "${PROJECT_ROOT}/${OUTPUT_FILE}" "${DIST_DIR}/${OUTPUT_FILE}"
