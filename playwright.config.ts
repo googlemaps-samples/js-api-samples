36c1f1e367b6d1e50cd87099f062feff4cc02f6b
William French <wfrench@google.com>
2025-04-09 12:37:22 -0700

    Update playwright.config.ts (#291)
    * Update playwright.config.ts
    
    Adds launchOptions clause to enable webGL in an attempt to fix some test breakage on 3D maps, which we suspect is caused by raster fallback on the CI side (all runs perfectly on local env).
    
    * Update playwright.config.ts
    
    * Update playwright.config.ts
    
    It's really really picky about commas.
    
    * Update playwright.config.ts
    
    add missing bracket

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,7 +59,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--enable-webgl', '--ignore-gpu-blocklist'],
+        },
+      },
     },
     /**
     {
