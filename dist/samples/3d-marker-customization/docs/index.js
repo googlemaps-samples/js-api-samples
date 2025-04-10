e4d3eb9187d2f2fe5ecec70c5e78a7430aa52991
William French <wfrench@google.com>
2025-04-10 09:01:06 -0700

    Update dist folder [skip ci] (#295)
    Co-authored-by: googlemaps-bot <googlemaps-bot@users.noreply.github.com>

---
+++ dist/samples/3d-marker-customization/docs/index.js
@@ -0,0 +1,95 @@
+"use strict";
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * * https://www.apache.org/licenses/LICENSE-2.0
+ * * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+// @ts-nocheck
+// [START maps_3d_marker_customization]
+async function init() {
+    const { Map3DElement, Marker3DElement } = await google.maps.importLibrary("maps3d");
+    const { PinElement } = await google.maps.importLibrary("marker");
+    const map = new Map3DElement({
+        center: { lat: 37.4176, lng: -122.02, altitude: 0 },
+        tilt: 67.5,
+        range: 7000,
+        mode: 'HYBRID'
+    });
+    map.mode = "SATELLITE";
+    // Change the border color.
+    const pinBorder = new PinElement({
+        borderColor: '#FFFFFF',
+    });
+    const markerWithBorder = new Marker3DElement({
+        position: { lat: 37.415, lng: -122.035 },
+    });
+    markerWithBorder.append(pinBorder);
+    // Add a label.
+    const markerWithLabel = new Marker3DElement({
+        position: { lat: 37.419, lng: -122.03 },
+        label: 'Simple label'
+    });
+    // Adjust the scale.
+    const pinScaled = new PinElement({
+        scale: 1.5,
+    });
+    const markerWithScale = new Marker3DElement({
+        position: { lat: 37.419, lng: -122.02 },
+    });
+    markerWithScale.append(pinScaled);
+    // Change the glyph color.
+    const pinGlyph = new PinElement({
+        glyphColor: 'white',
+    });
+    const markerWithGlyphColor = new Marker3DElement({
+        position: { lat: 37.415, lng: -122.025 },
+    });
+    markerWithGlyphColor.append(pinGlyph);
+    // Change many elements together and extrude marker.
+    const pinTextGlyph = new PinElement({
+        background: '#F0F6FC',
+        glyph: 'E',
+        glyphColor: 'red',
+        borderColor: '#0000FF',
+    });
+    const markerWithGlyphText = new Marker3DElement({
+        position: { lat: 37.415, lng: -122.015, altitude: 50 },
+        extruded: true,
+        altitudeMode: "RELATIVE_TO_GROUND",
+    });
+    markerWithGlyphText.append(pinTextGlyph);
+    // Hide the glyph.
+    const pinNoGlyph = new PinElement({
+        glyph: '',
+    });
+    const markerWithNoGlyph = new Marker3DElement({
+        position: { lat: 37.415, lng: -122.005 },
+    });
+    markerWithNoGlyph.append(pinNoGlyph);
+    // Change the background color.
+    const pinBackground = new PinElement({
+        background: '#FBBC04',
+    });
+    const markerWithBackground = new Marker3DElement({
+        position: { lat: 37.419, lng: -122.01 },
+    });
+    markerWithBackground.append(pinBackground);
+    map.append(markerWithLabel);
+    map.append(markerWithScale);
+    map.append(markerWithBackground);
+    map.append(markerWithBorder);
+    map.append(markerWithGlyphColor);
+    map.append(markerWithGlyphText);
+    map.append(markerWithNoGlyph);
+    document.body.append(map);
+}
+init();
+// [END maps_3d_marker_customization]
