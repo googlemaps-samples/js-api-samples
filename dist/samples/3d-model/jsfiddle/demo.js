72bde378eaefe751daea068269876011dc6595ed
William French <wfrench@google.com>
2025-04-21 15:22:11 -0700

    Update dist folder [skip ci] (#328)
    Co-authored-by: googlemaps-bot <googlemaps-bot@users.noreply.github.com>

--- dist/samples/3d-model/jsfiddle/demo.js
+++ dist/samples/3d-model/jsfiddle/demo.js
@@ -7,6 +7,7 @@
 // @ts-nocheck
 
 async function init() {
+    // Import the needed libraries.
     const { Map3DElement, Model3DElement } = await google.maps.importLibrary("maps3d");
     const map = new Map3DElement({
         center: { lat: 39.1178, lng: -106.4452, altitude: 4395.4952 }, range: 1500, tilt: 74, heading: 0,
