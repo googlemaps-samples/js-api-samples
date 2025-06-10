bfbfc80b9ed95827b7cf7945fc0df111b8dc479a
William French <wfrench@google.com>
2025-06-10 11:50:33 -0700

    feat: Adds script to find deleted subfolders.

---
+++ samples/test-example/index.ts
@@ -0,0 +1,35 @@
+/*
+ * @license
+ * Copyright 2025 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// [START maps_test_example]
+// Initialize and add the map.
+let map;
+async function initMap(): Promise<void> {
+  // [START maps_test_example_instantiate_map]
+  // The location of Uluru, Australia.
+  const position = {lat: -25.344, lng: 131.031};
+
+  //  Request the needed libraries.
+  const {Map} =
+      await google.maps.importLibrary('maps') as google.maps.MapsLibrary;
+  const {AdvancedMarkerElement} =
+      await google.maps.importLibrary('marker') as google.maps.MarkerLibrary;
+
+  // The map, centered at Uluru, Australia.
+  map = new Map(document.getElementById('map') as HTMLElement, {
+    zoom: 4,
+    center: position,
+    mapId: 'DEMO_MAP_ID',
+  });
+  // [END maps_test_example_instantiate_map]
+
+  // [START maps_test_example_instantiate_marker]
+  // The marker, positioned at Uluru.
+  const marker = new AdvancedMarkerElement({map, position, title: 'Uluru'});
+  // [END maps_test_example_instantiate_marker]  
+}
+initMap();
+// [END maps_test_example]
