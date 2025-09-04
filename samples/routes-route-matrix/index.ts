b7950e16bebbd32c6d096c43885f99862c388cfd
William French <wfrench@google.com>
2025-09-04 11:14:02 -0700

    Fix import type for routes library in initMap

--- samples/routes-route-matrix/index.ts
+++ samples/routes-route-matrix/index.ts
@@ -11,12 +11,12 @@ let center = { lat: 51.55, lng: -1.8 };
 
 async function initMap(): Promise<void> {
   //  Request the needed libraries.  
+  //@ts-ignore
   const [{Map}, {Place}, {AdvancedMarkerElement, PinElement}, {RouteMatrix}] = await Promise.all([
     google.maps.importLibrary('maps') as Promise<google.maps.MapsLibrary>,
     google.maps.importLibrary('places') as Promise<google.maps.PlacesLibrary>,
     google.maps.importLibrary('marker') as Promise<google.maps.MarkerLibrary>,
-    //@ts-ignore
-    google.maps.importLibrary('routes') as Promise<google.maps.Routes>
+    google.maps.importLibrary('routes') as Promise<google.maps.RoutesLibrary>
   ]);
 
   const bounds = new google.maps.LatLngBounds();
