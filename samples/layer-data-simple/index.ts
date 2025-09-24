60ef46131f23c71ca0a30dd960ce85ee7b85829e
William French <wfrench@google.com>
2025-09-24 10:21:44 -0700

    fix: Migrates the layer data sample; adds hardcoded data.
    Change-Id: I65d6a637af0e24fdd68aa0895aafc82ebb08c988

---
+++ samples/layer-data-simple/index.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// [START maps_layer_data_simple]
+const mapElement = document.querySelector('gmp-map') as google.maps.MapElement;
+let innerMap;
+
+async function initMap() {
+  await google.maps.importLibrary('maps') as google.maps.MapsLibrary;
+
+    innerMap = mapElement.innerMap;
+
+    google.maps.event.addListenerOnce(innerMap, 'idle', () => {
+      innerMap.data.loadGeoJson(
+         'https://maps-docs-team.web.app/assets/google.json'
+      );
+    });
+}
+
+initMap();
+// [END maps_layer_data_simple]
