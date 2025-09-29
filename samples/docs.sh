c33711d461ccdb16600f595c2109e494c6df3c34
William French <wfrench@google.com>
2025-09-29 12:10:50 -0700

    fix: Modify docs.sh to conditionally copy public folder (#795)
    This fixes an error. Updated the script to copy the 'public' folder only if it contains files.

--- samples/docs.sh
+++ samples/docs.sh
@@ -28,4 +28,6 @@ cp "${SCRIPT_DIR}/${NAME}/index.html" "${DOCS_DIR}/index.html"
 cp "${SCRIPT_DIR}/${NAME}/style.css" "${DOCS_DIR}/style.css"
 
 # Copy the data folder if one is found.
-# [ -d "public" ] && cp -r public/* "${DOCS_DIR}/"
+if [ -d "public" ] && [ "$(ls -A public)" ]; then
+  cp -r public/* "${DOCS_DIR}/"
+fi
