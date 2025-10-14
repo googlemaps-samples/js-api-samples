7de61552bb58add4e4ff87272131aa7abecf52a3
William French <wfrench@google.com>
2025-10-14 10:25:12 -0700

    Refactor draggable marker initialization in map
    Removed innerMap reference and updated draggableMarker initialization.

--- samples/advanced-markers-draggable/index.ts
+++ samples/advanced-markers-draggable/index.ts
@@ -6,35 +6,29 @@
 
 // [START maps_advanced_markers_draggable]
 const mapElement = document.querySelector('gmp-map') as google.maps.MapElement;
-let innerMap;
 
  async function initMap() {
     // Request needed libraries.
     const { Map, InfoWindow } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
-    
-    innerMap = mapElement.innerMap;
-    innerMap.setOptions({
-        mapTypeControl: false,
-    });
 
     const infoWindow = new InfoWindow();
 
     // [START maps_advanced_markers_draggable_marker]
     const draggableMarker = new AdvancedMarkerElement({
-        map: innerMap,
         position: {lat: 37.39094933041195, lng: -122.02503913145092},
         gmpDraggable: true,
         title: "This marker is draggable.",
     });
+    mapElement.append(draggableMarker);
     // [END maps_advanced_markers_draggable_marker]
+    
     draggableMarker.addListener('dragend', (event) => {
         const position = draggableMarker.position as google.maps.LatLng;
         infoWindow.close();
         infoWindow.setContent(`Pin dropped at: ${position.lat}, ${position.lng}`);
         infoWindow.open(draggableMarker.map, draggableMarker);
     });
-    
 }
 
 initMap();
