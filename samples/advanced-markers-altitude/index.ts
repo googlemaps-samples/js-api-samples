37f380ddbf7a71cca8d56b2b94e2afae4f6630a4
William French <wfrench@google.com>
2025-11-04 09:51:58 -0800

    Update copyright year and marker variable name

--- samples/advanced-markers-altitude/index.ts
+++ samples/advanced-markers-altitude/index.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
+ * Copyright 2025 Google LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -25,13 +25,13 @@ async function initMap() {
         scale: 2.0,
     });
 
-    const markerView = new AdvancedMarkerElement({
+    const marker = new AdvancedMarkerElement({
         content: pin.element,
         // Set altitude to 20 meters above the ground.
         position: { lat: 47.65170843460547, lng: -122.30754, altitude: 20 } as google.maps.LatLngAltitudeLiteral,
     });
 
-    mapElement.append(markerView);
+    mapElement.append(marker);
     // [END maps_advanced_markers_altitude_marker]
 }
 
