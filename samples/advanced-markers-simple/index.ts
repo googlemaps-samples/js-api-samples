ff162195b4fd396be5337dd349ece6010c1c59cc
William French <wfrench@google.com>
2025-11-04 07:47:38 -0800

    Refactor marker initialization in initMap function

--- samples/advanced-markers-simple/index.ts
+++ samples/advanced-markers-simple/index.ts
@@ -7,19 +7,16 @@
 // [START maps_advanced_markers_simple]
 // [START maps_advanced_markers_simple_snippet]
 const mapElement = document.querySelector('gmp-map') as google.maps.MapElement;
-let innerMap;
 
 async function initMap() {
     // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
-    innerMap = mapElement.innerMap;
-
     const marker = new AdvancedMarkerElement({
-        map: innerMap,
         position: { lat: 37.4239163, lng: -122.0947209 },
     });
+    mapElement.append(marker);
 }
 // [END maps_advanced_markers_simple_snippet]
 initMap();
