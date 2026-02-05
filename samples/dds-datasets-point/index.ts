cadcb6b6e640599feba4e51712ad0f0cb3abcc34
William French <wfrench@google.com>
2026-02-05 12:25:06 -0800

    Comment out datasetLayer.style assignment
    Commented out the line that sets the dataset layer style.

--- samples/dds-datasets-point/index.ts
+++ samples/dds-datasets-point/index.ts
@@ -62,7 +62,7 @@ async function initMap() {
     // Dataset ID for squirrel dataset.
     const datasetId = 'a99635b0-5e73-4b2a-8ae3-cb40f4b7f47e';
     const datasetLayer = innerMap.getDatasetFeatureLayer(datasetId);
-    datasetLayer.style = setStyle;
+    //datasetLayer.style = setStyle;
 }
 
 // Creates a legend for the map.
