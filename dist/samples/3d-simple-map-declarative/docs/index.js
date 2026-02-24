f8872f467e23125de32e8cd3af6a921539413554
anglarett <anglarett@google.com>
2026-02-24 11:59:40 +0100

    chore: remove build artifacts from tracking

--- dist/samples/3d-simple-map-declarative/docs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*
- * @license
- * Copyright 2025 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// @ts-nocheck
-// [START maps_3d_simple_map_declarative]
-// [END maps_3d_simple_map_declarative]
