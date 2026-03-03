8730f990da8e7a81f9c7b7253ae3b1fc1d9713e0
Ari Oppenheimer <ppenheimer@gmail.com>
2026-03-03 11:37:44 -0800

    Refactored flattener element to HTML

--- samples/3d-mesh-flatten/index.ts
+++ samples/3d-mesh-flatten/index.ts
@@ -5,14 +5,11 @@
  */
 
 // [START maps_3d_mesh_flatten]
-declare const path: { lat: number; lng: number; altitude: number }[];
-
 async function initMap(): Promise<void> {
     await google.maps.importLibrary('maps3d');
 
     const map = document.querySelector('gmp-map-3d') as Element;
-    const flattener = document.createElement('gmp-flattener') as any;
-    flattener.path = path;
+    const flattener = document.querySelector('gmp-flattener') as Element;
     map.append(flattener);
 
     const toggleButton = document.getElementById('toggleButton') as HTMLButtonElement;
