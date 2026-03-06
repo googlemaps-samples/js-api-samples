48978a5fe99e4fff53600306909c4e8df3eed965
William French <wfrench@google.com>
2026-03-06 07:44:02 -0800

    Update copyright year and add legacy comment (#1173)
    Updated copyright year from 2025 to 2026 and added legacy comment so we know this script works but is not used by any processes. Retaining for future use.

--- samples/upload-to-bucket.sh
+++ samples/upload-to-bucket.sh
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# “Legacy: previously invoked from .github/workflows/release-please.yml.
+# Removed when switching to release.yml/Firebase deploy. Kept for manual use.
 
 # Copy contents of /dist to the Cloud Bucket.
 
