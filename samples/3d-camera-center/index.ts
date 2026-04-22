1d135c85f4ea1a12f6077f1c34e21b7dc4ca2c85
William French <dubnewt@gmail.com>
2026-04-22 13:07:45 -0700

    feat: Refactors the 3d-camera-center sample.

--- samples/3d-camera-center/index.ts
+++ samples/3d-camera-center/index.ts
@@ -4,7 +4,40 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// @ts-nocheck
 // [START maps_3d_camera_center]
 
+async function initMap(): Promise<void> {
+    // Import the needed libraries.
+    await google.maps.importLibrary('maps3d');
+
+    // @ts-ignore
+    const map3DElement = document.querySelector('gmp-map-3d') as google.maps.Map3DElement;
+    const btn = document.getElementById('switch-mode-btn') as HTMLButtonElement;
+
+    const initialCenter = { lat: 40.7860524, lng: -73.9634983, altitude: 0 };
+    let isCenterMode = true;
+
+    btn.addEventListener('click', () => {
+        if (isCenterMode) {
+            // Switch to Camera Position Mode.
+            // Place the camera at the marker's location, but 50m up in the air
+            map3DElement.cameraPosition = { lat: initialCenter.lat, lng: initialCenter.lng, altitude: 50 };
+            map3DElement.tilt = 80;
+            map3DElement.heading = -150;
+            
+            btn.textContent = 'Switch to Center Mode';
+            isCenterMode = false;
+        } else {
+            // Revert back to Center Mode (looking AT the marker)
+            map3DElement.center = initialCenter;
+            map3DElement.range = 1500;
+            map3DElement.tilt = 70;
+
+            btn.textContent = 'Switch to Camera Position';
+            isCenterMode = true;
+        }
+    });
+}
+
+initMap();
 // [END maps_3d_camera_center]
