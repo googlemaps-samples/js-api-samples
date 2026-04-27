2fd0b87d3642792607fb43fc4f6a00c7bb039824
William French <dubnewt@gmail.com>
2026-04-27 10:03:57 -0700

    feat: Adds new legacy maps sample for div element.

---
+++ samples/map-simple-legacy/index.ts
@@ -0,0 +1,21 @@
+/*
+ * @license
+ * Copyright 2025 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// [START maps_map_simple]
+let map: google.maps.Map;
+async function initMap(): Promise<void> {
+    const { Map, RenderingType } = (await google.maps.importLibrary(
+        'maps'
+    )) as google.maps.MapsLibrary;
+    map = new Map(document.getElementById('map') as HTMLElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        renderingType: RenderingType.VECTOR,
+    });
+}
+
+initMap();
+// [END maps_map_simple]
