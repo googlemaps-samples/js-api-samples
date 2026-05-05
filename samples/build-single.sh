2ddf5261257937e28be38915a3863d1bd1f0f264
Chris J. Shull <chrisjshull@gmail.com>
2026-05-05 11:12:06 -0700

    DRY up build_single.sh

--- samples/build-single.sh
+++ samples/build-single.sh
@@ -8,32 +8,37 @@ NAME=$(basename $PWD)
 npx prettier --check --ignore-path ../../.prettierignore .
 npx eslint
 
+common_recursive_grep_options=( -r --exclude-dir=node_modules )
+all_type_recursive_grep_options=( "${common_recursive_grep_options[@]}" --include=*.{html,js,ts,css,jsx,tsx} --exclude-dir=dist )
+html_script_src_grep_options=( 'src="https://maps' "${common_recursive_grep_options[@]}"  --include=*.html )
+
 set +e
-grep weekly -r --include=*.{html,js,ts,css,jsx,tsx} --exclude-dir=dist --exclude-dir=node_modules
+grep weekly "${all_type_recursive_grep_options[@]}"
 if [[ $? -eq 0 ]]; then
   # sure, it could be other things, but so far it's not, so we'll keep it simple for now
   echo "Found 'weekly'. Please remove (it's the default channel)."
   exit 1
 fi
-grep callback -r --include=*.{html,js,ts,css,jsx,tsx} --exclude-dir=dist --exclude-dir=node_modules --no-filename | grep -v -E "^\s*//" | grep -v importLibrary # callback & importLibrary both show up the in the inline loader
+# callback & importLibrary both show up the in the inline loader, that's okay. comments also okay
+grep callback "${all_type_recursive_grep_options[@]}" | grep -v -E '^[-_./a-z0-9]+:\s*//' | grep -v importLibrary 
 if [[ $? -eq 0 ]]; then
   # sure, it could be other things, but so far it's not, so we'll keep it simple for now
   echo "Found 'callback'. Please replace with a more modern pattern for loading Maps JS."
   exit 1
 fi
-grep 'src="https://maps' -r --include=*.html --exclude-dir=node_modules
+grep 
 if [[ $? -eq 0 ]]; then
-  grep 'src="https://maps' -r --include=*.html --exclude-dir=node_modules | grep "loading=async"
+  grep "${html_script_src_grep_options[@]}" | grep "loading=async"
   if [[ $? -ne 0 ]]; then
     echo "Missing loading=async in direct script loader."
     exit 1
   fi
-  grep -B 1 'src="https://maps' -r --include=*.html --exclude-dir=node_modules | grep " async"
+  grep -B 1 "${html_script_src_grep_options[@]}" | grep " async"
   if [[ $? -ne 0 ]]; then
     echo "Missing async attribute on direct script loader (expected on line before)."
     exit 1
   fi
-  grep -B 2 -A 2 'src="https://maps' -r --include=*.html --exclude-dir=node_modules | grep " defer"
+  grep -B 2 -A 2 "${html_script_src_grep_options[@]}" | grep " defer"
   if [[ $? -eq 0 ]]; then
     echo "Found defer attribute on direct script loader (use async instead)."
     exit 1
