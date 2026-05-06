a76d55cd59a1cde8d92016b7d2504f921b215981
William French <dubnewt@gmail.com>
2026-05-06 10:18:42 -0700

    feat: Re-creates the 3d-map-45-degree-locked sample on clean branch.

---
+++ samples/3d-map-45-degree-locked/index.ts
@@ -0,0 +1,17 @@
+/*
+ * @license
+ * Copyright 2026 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// [START maps_3d_map_45_degree_locked]
+async function initMap(): Promise<void> {
+    const { Map3DElement } = await google.maps.importLibrary('maps3d');
+    await google.maps.importLibrary('maps3d');
+
+    const map3DElement = document.querySelector('gmp-map-3d')!;
+    
+}
+
+void initMap();
+// [END maps_3d_map_45_degree_locked]
