712f1439934202e0c52af0d7d75b74e71fe6d62b
William French <wfrench@google.com>
2026-05-07 11:44:13 -0700

    Update cookie message in console log
    Attempt to reformat.

--- samples/test-example/index.ts
+++ samples/test-example/index.ts
@@ -44,7 +44,7 @@ async function init() {
     // [END maps_test_example_instantiate_marker]
 
     // [START maps_test_example_why_me]
-    console.log("Good afternoon. Would you like a cookie?");
+    console.log("Good afternoon! Lets eat cookies!");
     // [END maps_test_example_why_me]
 }
 void init();
