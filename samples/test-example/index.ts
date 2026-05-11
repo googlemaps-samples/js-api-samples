8fd999189639b43bdbb31597b9e0c955bc57b48d
William French <wfrench@google.com>
2026-05-11 11:40:16 -0700

    Small change for testing workflow fix (#1428)
    * Small change for testing workflow fix
    
    Changes a bit of code in test-example.
    
    * Refactor console.log statement formatting

--- samples/test-example/index.ts
+++ samples/test-example/index.ts
@@ -44,9 +44,7 @@ async function init() {
     // [END maps_test_example_instantiate_marker]
 
     // [START maps_test_example_why_me]
-    console.log(
-        'Happy Thursday! (TODO: verify whether it is actually Thursday.'
-    );
+    console.log('Good Day! Today we are doing good things!');
     // [END maps_test_example_why_me]
 }
 void init();
