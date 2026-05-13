e155174c05cc3826f0b027a92873dd4450661348
Ari Oppenheimer <ppenheimer@gmail.com>
2026-05-13 10:45:24 -0700

    prettier fix in interactive sample

--- samples/3d-marker-interactive/index.ts
+++ samples/3d-marker-interactive/index.ts
@@ -26,14 +26,13 @@ async function init() {
         });
 
         popover.append(position.name);
-        
+
         // [START maps_3d_marker_interaction]
         const interactiveMarker = new Marker3DInteractiveElement({
             position,
             gmpPopoverTargetElement: popover,
         });
         // [END maps_3d_marker_interaction]
-
 
         map.append(interactiveMarker);
         map.append(popover);
