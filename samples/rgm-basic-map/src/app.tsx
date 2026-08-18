8c7e2453fafae9e3b9cb26ac2d721354627b5bf5
William French <wfrench@google.com>
2026-08-18 10:17:47 -0700

    feat: Migrate and modernize React basic map with marker (#1603)
    * feat: Migrate rgm-basic-map using react-web-components-markers
    
    Change-Id: I912f02dbeb3c0b5af018ce934b5ca6423ac1fedc
    
    * Applied prettier formatting.
    
    Change-Id: Id920215ca434b63cb32e808b8bb234e9249e9dc4
    
    * chore: Remove unused gmp-map CSS
    
    Change-Id: I31ac0152d90798742182cf15f4e7fd610558f126
    
    * Adds package-lock.json
    
    Change-Id: Id78905993db1844a45e08c3061ae70a0ae992bd7
    
    * fix: Align react and react-dom versions to 19.2.8 to fix Error 527
    
    Change-Id: Ic4d6705f0b11ce28b60ee4cd589bd56a1da6e9c3
    
    * chore: update package-lock.json
    
    Change-Id: I7e792cd72d5029171072e855588e550f242a29b3

---
+++ samples/rgm-basic-map/src/app.tsx
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// [START maps_rgm_basic_map]
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+
+const API_KEY = 'GOOGLE_MAPS_API_KEY';
+
+export default function App() {
+    return (
+        <APIProvider apiKey={API_KEY}>
+            <Map
+                defaultCenter={{ lat: 37.4220656, lng: -122.0840897 }}
+                defaultZoom={10}
+                mapId="DEMO_MAP_ID">
+                <AdvancedMarker
+                    position={{ lat: 37.4220656, lng: -122.0840897 }}
+                    title="Mountain View, CA"
+                />
+            </Map>
+        </APIProvider>
+    );
+}
+
+export function renderToDom(container: HTMLElement) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
+// [END maps_rgm_basic_map]
