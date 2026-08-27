79025364a32e192c9767196ca54ea6de0e331c9c
EJ Haselden <ejhaselden@google.com>
2026-08-27 00:20:26 +0000

    feat(samples): add advanced place details and search samples

---
+++ samples/ui-kit-advanced-place-search-nearby/index.ts
@@ -0,0 +1,125 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+ * @license
+ * Copyright 2025 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/* [START maps_ui_kit_advanced_place_search_nearby] */
+
+/* [START maps_ui_kit_advanced_place_search_nearby_query_selectors] */
+// Query selectors for various elements in the HTML file.
+const map = document.querySelector('gmp-map')!;
+const placeSearch: any = document.querySelector('gmp-advanced-place-search')!;
+const placeSearchQuery: any = document.querySelector(
+    'gmp-place-nearby-search-request'
+)!;
+const placeDetails = document.querySelector(
+    'gmp-advanced-place-details-compact'
+)!;
+const placeRequest: any = document.querySelector(
+    'gmp-place-details-place-request'
+)!;
+const typeSelect = document.querySelector('.type-select') as HTMLSelectElement;
+/* [END maps_ui_kit_advanced_place_search_nearby_query_selectors] */
+
+// Global variables for the map, markers, and info window.
+const markers = new Map<string, google.maps.marker.AdvancedMarkerElement>();
+let infoWindow: google.maps.InfoWindow;
+
+// The init function is called when the page loads.
+async function init(): Promise<void> {
+    // Import the necessary libraries from the Google Maps API.
+    const [{ InfoWindow }] = await Promise.all([
+        google.maps.importLibrary('maps'),
+        google.maps.importLibrary('places'),
+    ]);
+
+    // Create a new info window and set its content to the place details element.
+    placeDetails.remove(); // Hide the place details element because it is not needed until the info window opens
+    infoWindow = new InfoWindow({
+        content: placeDetails,
+        ariaLabel: 'Place Details',
+    });
+
+    // Set the map options.
+    map.innerMap.setOptions({
+        clickableIcons: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+    });
+
+    /* [START maps_ui_kit_advanced_place_search_nearby_event] */
+    // Add event listeners to the type select and place search elements.
+    typeSelect.addEventListener('change', () => {
+        searchPlaces();
+    });
+
+    placeSearch.addEventListener('gmp-select', (event) => {
+        const { place } = event;
+        markers.get(place.id)?.click();
+    });
+    placeSearch.addEventListener('gmp-load', () => {
+        void addMarkers();
+    });
+
+    searchPlaces();
+}
+/* [END maps_ui_kit_advanced_place_search_nearby_event] */
+/* [START maps_ui_kit_advanced_place_search_nearby_function] */
+// The searchPlaces function is called when the user changes the type select or when the page loads.
+function searchPlaces() {
+    // Close the info window and clear the markers.
+    infoWindow.close();
+    for (const marker of markers.values()) {
+        marker.remove();
+    }
+    markers.clear();
+
+    // Set the place search query and add an event listener to the place search element.
+    if (typeSelect.value) {
+        const center = map.center!;
+        placeSearchQuery.locationRestriction = {
+            center,
+            radius: 50000, // 50km radius
+        };
+        placeSearchQuery.includedTypes = [typeSelect.value];
+    }
+}
+/* [END maps_ui_kit_advanced_place_search_nearby_function] */
+
+// The addMarkers function is called when the place search element loads.
+async function addMarkers() {
+    // Import the necessary libraries from the Google Maps API.
+    const [{ AdvancedMarkerElement }, { LatLngBounds }] = await Promise.all([
+        google.maps.importLibrary('marker'),
+        google.maps.importLibrary('core'),
+    ]);
+    const bounds = new LatLngBounds();
+
+    if (placeSearch.places.length === 0) {
+        return;
+    }
+
+    for (const place of placeSearch.places) {
+        if (!place.location) continue;
+        const marker = new AdvancedMarkerElement({
+            map: map.innerMap,
+            position: place.location,
+            collisionBehavior: 'REQUIRED_AND_HIDES_OPTIONAL',
+        });
+
+        markers.set(place.id, marker);
+        bounds.extend(place.location);
+
+        marker.addListener('click', () => {
+            placeRequest.place = place;
+            infoWindow.open(map.innerMap, marker);
+        });
+    }
+
+    map.innerMap.fitBounds(bounds);
+}
+
+void init();
+/* [END maps_ui_kit_advanced_place_search_nearby] */
